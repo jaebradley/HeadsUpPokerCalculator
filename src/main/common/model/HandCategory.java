4f65dbf1904e189feb033a5e6d253f201fbfb906
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-18 11:19:15 -0500

    hand ranking

---
+++ src/main/common/model/HandCategory.java
@@ -0,0 +1,23 @@
+package main.common.model;
+
+public enum HandCategory {
+    HighCard(1),
+    OnePair(2),
+    TwoPair(3),
+    ThreeOfAKind(4),
+    Straight(5),
+    Flush(6),
+    FullHouse(7),
+    FourOfAKind(8),
+    StraightFlush(9);
+
+    private final int rankValue;
+
+    HandCategory(final int rankValue) {
+        this.rankValue = rankValue;
+    }
+
+    public int getRankValue() {
+        return rankValue;
+    }
+}
