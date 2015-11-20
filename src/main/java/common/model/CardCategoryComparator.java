bd49d8cf452b2ef6bef9e0f65eef977056f75771
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-19 22:23:22 -0500

    sorted card category returner impl

---
+++ src/main/java/common/model/CardCategoryComparator.java
@@ -0,0 +1,23 @@
+package main.java.common.model;
+
+import java.util.Comparator;
+
+public class CardCategoryComparator implements Comparator<CardCategory> {
+    @Override
+    public int compare(final CardCategory o1, final CardCategory o2) {
+        final int difference = o1.getValue() - o2.getValue();
+        if (difference > 0) {
+            return 1;
+        }
+
+        if (difference < 0) {
+            return -1;
+        }
+
+        if (0 == difference) {
+            return 0;
+        }
+
+        throw new RuntimeException("unexpected");
+    }
+}
---
+++ src/main/java/common/utils/impl/SortedCardCategoryReturnerImpl.java
@@ -0,0 +1,20 @@
+package main.java.common.utils.impl;
+
+import main.java.common.model.Card;
+import main.java.common.model.CardCategory;
+import main.java.common.model.Hand;
+import main.java.common.utils.interfaces.SortedCardCategoryReturner;
+
+import java.util.TreeSet;
+
+public class SortedCardCategoryReturnerImpl implements SortedCardCategoryReturner {
+    @Override
+    public TreeSet<CardCategory> returnCardCategoriesInAscendingOrder(final Hand hand) {
+        final TreeSet<CardCategory> sortedCardCategories = new TreeSet<>();
+        for (final Card card : hand.getCards()) {
+            sortedCardCategories.add(card.getCategory());
+        }
+
+        return sortedCardCategories;
+    }
+}
