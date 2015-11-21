c4b74d443b5cf67eed94e811881aa2004e22eebe
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-20 21:08:40 -0500

    exceptions for flush kicker calculator

--- src/main/java/kickersCalculator/impl/FlushKickersCalculatorImpl.java
+++ src/main/java/kickersCalculator/impl/FlushKickersCalculatorImpl.java
@@ -4,14 +4,23 @@ import main.java.common.model.CardCategory;
 import main.java.common.model.FlushKickers;
 import main.java.common.model.Hand;
 import main.java.common.utils.interfaces.SortedCardCategoryReturner;
+import main.java.kickersCalculator.exceptions.HandDoesNotContainFiveDistinctCardCategoriesException;
 import main.java.kickersCalculator.interfaces.FlushKickersCalculator;
 
 import java.util.TreeSet;
 
 public class FlushKickersCalculatorImpl implements FlushKickersCalculator {
     @Override
-    public FlushKickers calculateKickers(final Hand hand, final SortedCardCategoryReturner sortedCardCategoryReturner) {
+    public FlushKickers calculateKickers(
+            final Hand hand,
+            final SortedCardCategoryReturner sortedCardCategoryReturner
+    ) throws HandDoesNotContainFiveDistinctCardCategoriesException {
         final TreeSet<CardCategory> cardCategoriesSet = sortedCardCategoryReturner.returnCardCategoriesInAscendingOrder(hand);
+
+        if (5 != cardCategoriesSet.size()) {
+            throw new HandDoesNotContainFiveDistinctCardCategoriesException();
+        }
+
         final CardCategory[] cardCategories = cardCategoriesSet.toArray(new CardCategory[cardCategoriesSet.size()]);
         return new FlushKickers(
                 cardCategories[cardCategories.length - 1],
--- src/main/java/kickersCalculator/interfaces/FlushKickersCalculator.java
+++ src/main/java/kickersCalculator/interfaces/FlushKickersCalculator.java
@@ -3,7 +3,11 @@ package main.java.kickersCalculator.interfaces;
 import main.java.common.model.FlushKickers;
 import main.java.common.model.Hand;
 import main.java.common.utils.interfaces.SortedCardCategoryReturner;
+import main.java.kickersCalculator.exceptions.HandDoesNotContainFiveDistinctCardCategoriesException;
 
 public interface FlushKickersCalculator {
-    FlushKickers calculateKickers(final Hand hand, final SortedCardCategoryReturner sortedCardCategoryReturner) throws IncorrectNumberOfCardCategoriesException;
+    FlushKickers calculateKickers(
+            final Hand hand,
+            final SortedCardCategoryReturner sortedCardCategoryReturner
+    ) throws HandDoesNotContainFiveDistinctCardCategoriesException;
 }
