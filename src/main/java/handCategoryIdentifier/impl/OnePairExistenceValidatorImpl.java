02c60c1445b0e4e5df2778ea5fbf4c83c9c1417c
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-22 11:03:41 -0500

    one pair existence validator

--- src/main/java/handCategoryIdentifier/impl/OnePairExistenceValidatorImpl.java
+++ src/main/java/handCategoryIdentifier/impl/OnePairExistenceValidatorImpl.java
@@ -2,11 +2,19 @@ package main.java.handCategoryIdentifier.impl;
 
 import main.java.common.model.Hand;
 import main.java.common.utils.impl.CardCategoryPairCounterImpl;
+import main.java.common.utils.interfaces.CardCategoryPairCounter;
 import main.java.handCategoryIdentifier.interfaces.HandCategoryExistenceValidator;
+import main.java.handCategoryIdentifier.interfaces.OnePairExistenceValidator;
 
-public class OnePairExistenceValidatorImpl implements HandCategoryExistenceValidator {
+public class OnePairExistenceValidatorImpl implements OnePairExistenceValidator {
+    private final CardCategoryPairCounter cardCategoryPairCounter;
+
+    public OnePairExistenceValidatorImpl(final CardCategoryPairCounter cardCategoryPairCounter) {
+        this.cardCategoryPairCounter = cardCategoryPairCounter;
+    }
+
     @Override
-    public boolean validateExistence(final Hand hand) {
-        return 1 == CardCategoryPairCounterImpl.countCardCategoryPairs(hand);
+    public boolean onePairExists(final Hand hand) {
+        return 1 == cardCategoryPairCounter.countCardCategoryPairs(hand);
     }
 }
---
+++ src/main/java/handCategoryIdentifier/interfaces/OnePairExistenceValidator.java
@@ -0,0 +1,7 @@
+package main.java.handCategoryIdentifier.interfaces;
+
+import main.java.common.model.Hand;
+
+public interface OnePairExistenceValidator {
+    boolean onePairExists(final Hand hand);
+}
