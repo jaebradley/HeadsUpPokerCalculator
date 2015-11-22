265b321529299f927b719af3925215a2d557eefd
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-22 11:05:46 -0500

    straight existence validator

--- src/main/java/handCategoryIdentifier/impl/StraightExistenceValidatorImpl.java
+++ src/main/java/handCategoryIdentifier/impl/StraightExistenceValidatorImpl.java
@@ -3,15 +3,23 @@ package main.java.handCategoryIdentifier.impl;
 import main.java.common.model.CardCategory;
 import main.java.common.model.Hand;
 import main.java.common.utils.impl.SortedCardCategoryReturnerImpl;
+import main.java.common.utils.interfaces.SortedCardCategoryReturner;
+import main.java.handCategoryIdentifier.interfaces.StraightExistenceValidator;
 
 import java.util.TreeSet;
 
-public class StraightExistenceValidatorImpl {
-    public boolean validateExistence(final Hand hand) {
-        final TreeSet<CardCategory> cardCategories = SortedCardCategoryReturnerImpl.returnCardCategoriesInAscendingOrder(hand);
+public class StraightExistenceValidatorImpl implements StraightExistenceValidator {
+    private final SortedCardCategoryReturner sortedCardCategoryReturner;
+
+    public StraightExistenceValidatorImpl(final SortedCardCategoryReturner sortedCardCategoryReturner) {
+        this.sortedCardCategoryReturner = sortedCardCategoryReturner;
+    }
+
+    public boolean straightExists(final Hand hand) {
+        final TreeSet<CardCategory> cardCategories = sortedCardCategoryReturner.returnCardCategoriesInAscendingOrder(hand);
         return  (5 == cardCategories.size()) &&
                 (
-                        (
+                        (s
                                 cardCategories.contains(CardCategory.ACE) &&
                                 cardCategories.contains(CardCategory.TWO) &&
                                 cardCategories.contains(CardCategory.THREE) &&
---
+++ src/main/java/handCategoryIdentifier/interfaces/StraightExistenceValidator.java
@@ -0,0 +1,7 @@
+package main.java.handCategoryIdentifier.interfaces;
+
+import main.java.common.model.Hand;
+
+public interface StraightExistenceValidator {
+    boolean straightExists(final Hand hand);
+}
