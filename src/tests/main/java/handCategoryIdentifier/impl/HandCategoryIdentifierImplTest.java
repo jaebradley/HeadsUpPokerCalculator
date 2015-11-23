62d91a9b90d200be17a5fda54abf00d523eb2de3
Jae Bradley <jae.b.bradley@gmail.com>
2015-11-23 00:01:13 -0500

    hand category identifier implementation test

--- src/tests/main/java/handCategoryIdentifier/impl/HandCategoryIdentifierImplTest.java
+++ src/tests/main/java/handCategoryIdentifier/impl/HandCategoryIdentifierImplTest.java
@@ -219,4 +219,171 @@ public class HandCategoryIdentifierImplTest {
         );
     }
 
+    @Test
+    public void testFullHouse() {
+        final HandCategoryIdentifierImpl handCategoryIdentifier1 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindExistenceValidator,
+                twoPairExistenceValidator,
+                onePairExistenceValidator
+        );
+
+        final HandCategoryIdentifierImpl handCategoryIdentifier2 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindExistenceValidator,
+                twoPairNonExistenceValidator,
+                onePairExistenceValidator
+        );
+
+        final HashSet<Card> cardHashSet = new HashSet<>();
+        cardHashSet.add(new Card(CardCategory.ACE, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TWO, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.JACK, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.DIAMONDS));
+        final Hand fooHand = new Hand(cardHashSet);
+
+        Assert.assertEquals(
+                HandCategory.FullHouse,
+                handCategoryIdentifier1.identifyHandCategory(fooHand)
+        );
+
+        Assert.assertEquals(
+                HandCategory.FullHouse,
+                handCategoryIdentifier2.identifyHandCategory(fooHand)
+        );
+    }
+
+    @Test
+    public void testThreeOfAKind() {
+        final HandCategoryIdentifierImpl handCategoryIdentifier1 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindExistenceValidator,
+                twoPairExistenceValidator,
+                onePairNonExistenceValidator
+        );
+
+        final HandCategoryIdentifierImpl handCategoryIdentifier2 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindExistenceValidator,
+                twoPairNonExistenceValidator,
+                onePairNonExistenceValidator
+        );
+
+        final HashSet<Card> cardHashSet = new HashSet<>();
+        cardHashSet.add(new Card(CardCategory.ACE, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TWO, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.JACK, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.DIAMONDS));
+        final Hand fooHand = new Hand(cardHashSet);
+
+        Assert.assertEquals(
+                HandCategory.ThreeOfAKind,
+                handCategoryIdentifier1.identifyHandCategory(fooHand)
+        );
+
+        Assert.assertEquals(
+                HandCategory.ThreeOfAKind,
+                handCategoryIdentifier2.identifyHandCategory(fooHand)
+        );
+    }
+
+    @Test
+    public void testTwoPair() {
+        final HandCategoryIdentifierImpl handCategoryIdentifier1 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindNonExistenceValidator,
+                twoPairExistenceValidator,
+                onePairExistenceValidator
+        );
+
+        final HandCategoryIdentifierImpl handCategoryIdentifier2 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindNonExistenceValidator,
+                twoPairExistenceValidator,
+                onePairNonExistenceValidator
+        );
+
+        final HashSet<Card> cardHashSet = new HashSet<>();
+        cardHashSet.add(new Card(CardCategory.ACE, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TWO, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.JACK, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.DIAMONDS));
+        final Hand fooHand = new Hand(cardHashSet);
+
+        Assert.assertEquals(
+                HandCategory.TwoPair,
+                handCategoryIdentifier1.identifyHandCategory(fooHand)
+        );
+
+        Assert.assertEquals(
+                HandCategory.TwoPair,
+                handCategoryIdentifier2.identifyHandCategory(fooHand)
+        );
+    }
+
+    @Test
+    public void testOnePair() {
+        final HandCategoryIdentifierImpl handCategoryIdentifier1 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindNonExistenceValidator,
+                twoPairNonExistenceValidator,
+                onePairExistenceValidator
+        );
+
+        final HashSet<Card> cardHashSet = new HashSet<>();
+        cardHashSet.add(new Card(CardCategory.ACE, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TWO, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.JACK, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.DIAMONDS));
+        final Hand fooHand = new Hand(cardHashSet);
+
+        Assert.assertEquals(
+                HandCategory.OnePair,
+                handCategoryIdentifier1.identifyHandCategory(fooHand)
+        );
+    }
+
+    @Test
+    public void testHighCard() {
+        final HandCategoryIdentifierImpl handCategoryIdentifier1 = new HandCategoryIdentifierImpl(
+                flushNonExistenceValidator,
+                straightNonExistenceValidator,
+                fourOfAKindNonExistenceValidator,
+                threeOfAKindNonExistenceValidator,
+                twoPairNonExistenceValidator,
+                onePairNonExistenceValidator
+        );
+
+        final HashSet<Card> cardHashSet = new HashSet<>();
+        cardHashSet.add(new Card(CardCategory.ACE, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TWO, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.JACK, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.SPADES));
+        cardHashSet.add(new Card(CardCategory.TEN, Suit.DIAMONDS));
+        final Hand fooHand = new Hand(cardHashSet);
+
+        Assert.assertEquals(
+                HandCategory.HighCard,
+                handCategoryIdentifier1.identifyHandCategory(fooHand)
+        );
+    }
+
 }
